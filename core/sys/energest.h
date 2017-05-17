8cc5e082acaa5198c7c1b2c057ed4ad0470d586c
Simon Duquennoy <simon.duquennoy@gmail.com>
2017-05-17 13:56:30 +0200

    Removing more refs to old platforms

--- core/sys/energest.h
+++ core/sys/energest.h
@@ -90,37 +90,7 @@ extern energest_t energest_leveldevice_current_leveltime[ENERGEST_CONF_LEVELDEVI
                            energest_current_time[type] = RTIMER_NOW(); \
 			   energest_current_mode[type] = 1; \
                            } while(0)
-#ifdef __AVR__
-/* Handle 16 bit rtimer wraparound */
-#define ENERGEST_OFF(type) if(energest_current_mode[type] != 0) do {	\
-							if (RTIMER_NOW() < energest_current_time[type]) energest_total_time[type].current += RTIMER_ARCH_SECOND; \
-							energest_total_time[type].current += (rtimer_clock_t)(RTIMER_NOW() - \
-							energest_current_time[type]); \
-							energest_current_mode[type] = 0; \
-                           } while(0)
 
-#define ENERGEST_OFF_LEVEL(type,level) do { \
-										if (RTIMER_NOW() < energest_current_time[type]) energest_total_time[type].current += RTIMER_ARCH_SECOND; \
-										energest_leveldevice_current_leveltime[level].current += (rtimer_clock_t)(RTIMER_NOW() - \
-										energest_current_time[type]); \
-										energest_current_mode[type] = 0; \
-                                       } while(0)
-
-#define ENERGEST_SWITCH(type_off, type_on) do { \
-                                             rtimer_clock_t energest_local_variable_now = RTIMER_NOW(); \
-                                             if(energest_current_mode[type_off] != 0) { \
-                                               if (energest_local_variable_now < energest_current_time[type_off]) { \
-                                                 energest_total_time[type_off].current += RTIMER_ARCH_SECOND; \
-                                               } \
-                                               energest_total_time[type_off].current += (rtimer_clock_t)(energest_local_variable_now - \
-                                                 energest_current_time[type_off]); \
-                                               energest_current_mode[type_off] = 0; \
-                                             } \
-                                             energest_current_time[type_on] = energest_local_variable_now; \
-                                             energest_current_mode[type_on] = 1; \
-                                           } while(0)
-
-#else
 #define ENERGEST_OFF(type) if(energest_current_mode[type] != 0) do {	\
                            energest_total_time[type].current += (rtimer_clock_t)(RTIMER_NOW() - \
                            energest_current_time[type]); \
@@ -143,7 +113,6 @@ extern energest_t energest_leveldevice_current_leveltime[ENERGEST_CONF_LEVELDEVI
                                              energest_current_time[type_on] = energest_local_variable_now; \
                                              energest_current_mode[type_on] = 1; \
                                            } while(0)
-#endif
 
 #else /* ENERGEST_CONF_ON */
 #define ENERGEST_ON(type) do { } while(0)
