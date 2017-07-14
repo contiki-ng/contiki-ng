2817b9212a43165ee4f854dca80639f3dfb332df
Simon Duquennoy <simon.duquennoy@gmail.com>
2017-07-14 16:10:03 +0200

    framer-802154: compress PANID by default

--- os/net/mac/framer/framer-802154.c
+++ os/net/mac/framer/framer-802154.c
@@ -148,10 +148,8 @@ framer_802154_setup_params(packetbuf_attr_t (*get_attr)(uint8_t type),
     params->fcf.ack_required = get_attr(PACKETBUF_ATTR_MAC_ACK);
     params->fcf.sequence_number_suppression = FRAME802154_SUPPR_SEQNO;
   }
-  /* We do not compress PAN ID in outgoing frames, i.e. include one PAN ID (dest by default)
-   * There is one exception, seemingly a typo in Table 2a: rows 2 and 3: when there is no
-   * source nor destination address, we have dest PAN ID iff compression is *set*. */
-  params->fcf.panid_compression = 0;
+  /* Compress PAN ID in outgoing frames by default */
+  params->fcf.panid_compression = 1;
 
   /* Set IE Present bit */
   params->fcf.ie_list_present = get_attr(PACKETBUF_ATTR_MAC_METADATA);
