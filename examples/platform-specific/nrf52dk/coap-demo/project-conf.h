a2e74bad8b081779a922dc22a6ff633b02e42ab8
George Oikonomou <george@contiki-os.org>
2017-10-07 16:04:04 +0100

    Do not #undef in example project-conf.h (nrf52dk)

--- examples/platform-specific/nrf52dk/coap-demo/project-conf.h
+++ examples/platform-specific/nrf52dk/coap-demo/project-conf.h
@@ -46,33 +46,26 @@
 #define __PROJECT_ERBIUM_CONF_H__
 
 /* Disabling TCP on CoAP nodes. */
-#undef UIP_CONF_TCP
 #define UIP_CONF_TCP                   0
 
 /* Increase rpl-border-router IP-buffer when using more than 64. */
-#undef REST_MAX_CHUNK_SIZE
 #define REST_MAX_CHUNK_SIZE            48
 
 /* Estimate your header size, especially when using Proxy-Uri. */
 /*
-   #undef COAP_MAX_HEADER_SIZE
    #define COAP_MAX_HEADER_SIZE           70
  */
 
 /* Multiplies with chunk size, be aware of memory constraints. */
-#undef COAP_MAX_OPEN_TRANSACTIONS
 #define COAP_MAX_OPEN_TRANSACTIONS     4
 
 /* Must be <= open transactions, default is COAP_MAX_OPEN_TRANSACTIONS-1. */
 /*
-   #undef COAP_MAX_OBSERVERS
    #define COAP_MAX_OBSERVERS             2
  */
 
 /* Filtering .well-known/core per query can be disabled to save space. */
-#undef COAP_LINK_FORMAT_FILTERING
 #define COAP_LINK_FORMAT_FILTERING     0
-#undef COAP_PROXY_OPTION_PROCESSING
 #define COAP_PROXY_OPTION_PROCESSING   0
 
 /* Enable client-side support for COAP observe */
