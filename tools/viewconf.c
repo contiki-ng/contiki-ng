f35c15cf904e4cc803d760629efd2b6fab8f1545
Simon Duquennoy <simon.duquennoy@gmail.com>
2017-10-12 14:58:58 +0200

    Make viewconf more TSCH-friendly

--- tools/viewconf.c
+++ tools/viewconf.c
@@ -2,6 +2,7 @@
 #include "os/net/mac/framer/frame802154.h"
 #include "os/net/mac/tsch/tsch.h"
 #include "os/net/mac/tsch/tsch-conf.h"
+#include "os/net/mac/tsch/tsch-schedule.h"
 #include "os/net/ipv6/uip-nd6.h"
 #include "os/net/ipv6/uipopt.h"
 #include "os/net/queuebuf.h"
@@ -12,10 +13,16 @@
 ##### "CONTIKI_VERSION_STRING": ________________ CONTIKI_VERSION_STRING
 ##### "IEEE802154_PANID":_______________________ IEEE802154_PANID
 ##### "FRAME802154_VERSION":____________________ FRAME802154_VERSION
-##### "RF_CHANNEL": ____________________________ RF_CHANNEL
+#if MAC_CONF_WITH_TSCH
 ##### "TSCH_DEFAULT_HOPPING_SEQUENCE": _________ TSCH_DEFAULT_HOPPING_SEQUENCE
 ##### "TSCH_JOIN_HOPPING_SEQUENCE": ____________ TSCH_JOIN_HOPPING_SEQUENCE
+##### "TSCH_EB_PERIOD": ________________________ TSCH_EB_PERIOD
+##### "TSCH_MAX_EB_PERIOD": ____________________ TSCH_MAX_EB_PERIOD
 ##### "TSCH_CONF_DEFAULT_TIMESLOT_LENGTH": _____ TSCH_CONF_DEFAULT_TIMESLOT_LENGTH
+##### "TSCH_SCHEDULE_DEFAULT_LENGTH": __________ TSCH_SCHEDULE_DEFAULT_LENGTH
+#else /* MAC_CONF_WITH_TSCH */
+##### "RF_CHANNEL": ____________________________ RF_CHANNEL
+#endif /*MAC_CONF_WITH_TSCH */
 ##### "QUEUEBUF_NUM": __________________________ QUEUEBUF_CONF_NUM
 ##### "NBR_TABLE_MAX_NEIGHBORS": _______________ NBR_TABLE_CONF_MAX_NEIGHBORS
 ##### "NETSTACK_MAX_ROUTE_ENTRIES": ____________ NETSTACK_MAX_ROUTE_ENTRIES
