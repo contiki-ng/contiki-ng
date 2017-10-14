0ca938245f3cd664509c820423ab84bdf7c05780
George Oikonomou <george@contiki-os.org>
2017-10-14 19:06:37 +0100

    Add implementation of the common main routine

---
+++ os/contiki-main.c
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2017, George Oikonomou - http://www.spd.gr
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*---------------------------------------------------------------------------*/
+/**
+ * \addtogroup main
+ * @{
+ */
+/*---------------------------------------------------------------------------*/
+/**
+ * \file
+ *
+ * Implementation of Contiki-NG's main routine
+ */
+/*---------------------------------------------------------------------------*/
+#include "contiki.h"
+#include "contiki-net.h"
+#include "sys/platform.h"
+#include "net/queuebuf.h"
+#include "dev/watchdog.h"
+
+#if BUILD_WITH_ORCHESTRA
+#include "os/services/orchestra/orchestra.h"
+#endif /* BUILD_WITH_ORCHESTRA */
+#if BUILD_WITH_SHELL
+#include "os/services/shell/serial-shell.h"
+#endif /* BUILD_WITH_SHELL */
+
+#include <stdio.h>
+#include <stdint.h>
+/*---------------------------------------------------------------------------*/
+int
+main(void)
+{
+  platform_init_stage_one();
+
+  clock_init();
+  rtimer_init();
+  process_init();
+  process_start(&etimer_process, NULL);
+  ctimer_init();
+
+  energest_init();
+  ENERGEST_ON(ENERGEST_TYPE_CPU);
+
+  platform_init_stage_two();
+
+#if PLATFORM_STARTUP_VERBOSE
+  printf("Starting " CONTIKI_VERSION_STRING "\n");
+
+  printf(" Net: ");
+  printf("%s\n", NETSTACK_NETWORK.name);
+  printf(" MAC: ");
+  printf("%s\n", NETSTACK_MAC.name);
+#endif
+
+  netstack_init();
+
+  platform_init_stage_three();
+
+#if NETSTACK_CONF_WITH_IPV6
+  memcpy(&uip_lladdr.addr, &linkaddr_node_addr, sizeof(uip_lladdr.addr));
+  queuebuf_init();
+  process_start(&tcpip_process, NULL);
+#endif /* NETSTACK_CONF_WITH_IPV6 */
+
+#if BUILD_WITH_ORCHESTRA
+  orchestra_init();
+#endif /* BUILD_WITH_ORCHESTRA */
+
+#if BUILD_WITH_SHELL
+  serial_shell_init();
+#endif /* BUILD_WITH_SHELL */
+
+  autostart_start(autostart_processes);
+
+  watchdog_start();
+
+  while(1) {
+    uint8_t r;
+    do {
+      r = process_run();
+      watchdog_periodic();
+    } while(r > 0);
+
+    platform_idle();
+  }
+}
+/*---------------------------------------------------------------------------*/
+/**
+ * @}
+ */
