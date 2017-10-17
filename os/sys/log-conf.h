e9509be98cc35359a7c08395eeb39b325231cc42
George Oikonomou <george@contiki-os.org>
2017-10-18 00:16:11 +0100

    Extend the logging module to support the common main + platform-specific hooks

--- os/sys/log-conf.h
+++ os/sys/log-conf.h
@@ -117,6 +117,10 @@
 #define LOG_CONF_LEVEL_6TOP                        LOG_LEVEL_NONE
 #endif /* LOG_CONF_LEVEL_6TOP */
 
+#ifndef LOG_CONF_LEVEL_MAIN
+#define LOG_CONF_LEVEL_MAIN                        LOG_LEVEL_INFO
+#endif /* LOG_CONF_LEVEL_MAIN */
+
 #endif /* __LOG_CONF_H__ */
 
 /** @} */
