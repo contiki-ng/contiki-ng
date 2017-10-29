e1df7be4963b28af38ee7af49f880b684f62c3f0
George Oikonomou <george@contiki-ng.org>
2017-10-29 04:21:48 +0000

    Change slip_arch_init prototype
    All current platforms ignore the ubr argument, so we remove it

--- os/dev/slip.h
+++ os/dev/slip.h
@@ -80,7 +80,7 @@ void slip_set_input_callback(void (*callback)(void));
  * These machine dependent functions and an interrupt service routine
  * must be provided externally (slip_arch.c).
  */
-void slip_arch_init(unsigned long ubr);
+void slip_arch_init(void);
 void slip_arch_writeb(unsigned char c);
 
 #endif /* SLIP_H_ */
