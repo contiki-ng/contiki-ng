3460a4c735546cabe8976d7ff6968cfa882016c8
George Oikonomou <george@contiki-ng.org>
2017-10-29 03:06:14 +0000

    Don't assume the platform will provide uart1.h

--- examples/rpl-border-router/slip-bridge.c
+++ examples/rpl-border-router/slip-bridge.c
@@ -41,7 +41,6 @@
 #include "net/ipv6/uip.h"
 #include "net/ipv6/uip-ds6.h"
 #include "dev/slip.h"
-#include "dev/uart1.h"
 #include <string.h>
 
 #define UIP_IP_BUF        ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
