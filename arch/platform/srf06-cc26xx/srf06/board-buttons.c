3f4fef4625b8f223d89cfcc777ecb634f4861045
George Oikonomou <george@contiki-ng.org>
2017-11-04 22:59:07 +0000

    Implement the new button HAL: SmartRF06 + CC13xxEM/CC26xxEM

--- arch/platform/srf06-cc26xx/srf06/board-buttons.c
+++ arch/platform/srf06-cc26xx/srf06/board-buttons.c
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) 2014, Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (c) 2017, George Oikonomou - http://www.spd.gr
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
@@ -33,28 +34,30 @@
  * @{
  *
  * \file
- * Header file for the SmartRF06EB + CC13xx/CC26xxEM Button Driver
+ * Defines SmarfRF06 + CC13xxEM/CC26xxEM buttons for use with the button HAL
  */
 /*---------------------------------------------------------------------------*/
-#ifndef BUTTON_SENSOR_H_
-#define BUTTON_SENSOR_H_
-/*---------------------------------------------------------------------------*/
-#include "lib/sensors.h"
-/*---------------------------------------------------------------------------*/
-#define BUTTON_SENSOR "Button"
-/*---------------------------------------------------------------------------*/
-#define BUTTON_SENSOR_VALUE_STATE    0
-#define BUTTON_SENSOR_VALUE_DURATION 1
+#include "contiki.h"
+#include "dev/button-hal.h"
 
-#define BUTTON_SENSOR_VALUE_RELEASED 0
-#define BUTTON_SENSOR_VALUE_PRESSED  1
+#include "ti-lib.h"
 /*---------------------------------------------------------------------------*/
-extern const struct sensors_sensor button_select_sensor;
-extern const struct sensors_sensor button_left_sensor;
-extern const struct sensors_sensor button_right_sensor;
-extern const struct sensors_sensor button_up_sensor;
-extern const struct sensors_sensor button_down_sensor;
+BUTTON_HAL_BUTTON(key_left, "Key Left", BOARD_IOID_KEY_LEFT, \
+                  GPIO_HAL_PIN_CFG_PULL_UP, BOARD_BUTTON_HAL_INDEX_KEY_LEFT, \
+                  true);
+BUTTON_HAL_BUTTON(key_right, "Key Right", BOARD_IOID_KEY_RIGHT, \
+                  GPIO_HAL_PIN_CFG_PULL_UP, BOARD_BUTTON_HAL_INDEX_KEY_RIGHT, \
+                  true);
+BUTTON_HAL_BUTTON(key_up, "Key Up", BOARD_IOID_KEY_UP, \
+                  GPIO_HAL_PIN_CFG_PULL_UP, BOARD_BUTTON_HAL_INDEX_KEY_UP, \
+                  true);
+BUTTON_HAL_BUTTON(key_down, "Key Down", BOARD_IOID_KEY_DOWN, \
+                  GPIO_HAL_PIN_CFG_PULL_UP, BOARD_BUTTON_HAL_INDEX_KEY_DOWN, \
+                  true);
+BUTTON_HAL_BUTTON(key_select, "Key Select", BOARD_IOID_KEY_SELECT, \
+                  GPIO_HAL_PIN_CFG_PULL_UP, \
+                  BOARD_BUTTON_HAL_INDEX_KEY_SELECT, true);
 /*---------------------------------------------------------------------------*/
-#endif /* BUTTON_SENSOR_H_ */
+BUTTON_HAL_BUTTONS(&key_left, &key_right, &key_up, &key_down, &key_select);
 /*---------------------------------------------------------------------------*/
 /** @} */
