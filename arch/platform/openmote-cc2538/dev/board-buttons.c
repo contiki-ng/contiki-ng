d22ea1df9991aad79b065e62889e01c71d011464
George Oikonomou <george@contiki-ng.org>
2017-11-05 18:46:35 +0000

    Use the new button HAL: OpenMote-CC2538

--- arch/platform/openmote-cc2538/dev/board-buttons.c
+++ arch/platform/openmote-cc2538/dev/board-buttons.c
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2012, Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (c) 2017, George Oikonomou - http://www.spd.gr
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- *
  * 3. Neither the name of the copyright holder nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
@@ -27,46 +27,29 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
  */
 /*---------------------------------------------------------------------------*/
 /**
  * \addtogroup openmote-cc2538
  * @{
  *
- * \defgroup openmote-button-sensor OpenMote-CC2538 user button driver
+ * \defgroup openmote-cc2538-buttons OpenMote-CC2538 user button
  *
- * The user button will generate a sensors_changed event on press as
- * well as on release.
- *
+ * Generic module controlling the user button on the OpenMote-CC2538
  * @{
  *
  * \file
- * Header for the OpenMote-CC2538 button driver
+ * Defines the OpenMote-CC2538 user button for use with the button HAL
  */
 /*---------------------------------------------------------------------------*/
-#ifndef BUTTON_SENSOR_H_
-#define BUTTON_SENSOR_H_
+#include "contiki.h"
+#include "dev/button-hal.h"
 /*---------------------------------------------------------------------------*/
-#include "lib/sensors.h"
+BUTTON_HAL_BUTTON(button_user, "User button", \
+                  GPIO_PORT_PIN_TO_GPIO_HAL_PIN(BUTTON_USER_PORT, BUTTON_USER_PIN), \
+                  GPIO_HAL_PIN_CFG_PULL_UP, BUTTON_HAL_ID_USER_BUTTON, true);
 /*---------------------------------------------------------------------------*/
-#define BUTTON_SENSOR "Button"
-
-extern const struct sensors_sensor button_sensor;
-/*---------------------------------------------------------------------------*/
-extern process_event_t button_press_duration_exceeded;
-/*---------------------------------------------------------------------------*/
-#define BUTTON_SENSOR_CONFIG_TYPE_INTERVAL      0x0100
-
-#define BUTTON_SENSOR_VALUE_TYPE_LEVEL          0
-#define BUTTON_SENSOR_VALUE_TYPE_PRESS_DURATION 1
-
-#define BUTTON_SENSOR_PRESSED_LEVEL             0
-#define BUTTON_SENSOR_RELEASED_LEVEL            8
-/*---------------------------------------------------------------------------*/
-#endif /* BUTTON_SENSOR_H_ */
+BUTTON_HAL_BUTTONS(&button_user);
 /*---------------------------------------------------------------------------*/
 /**
  * @}
