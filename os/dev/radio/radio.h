ff16a5033e8b1decb57f911cc4aec2169ab44712
George Oikonomou <george@contiki-ng.org>
2017-12-16 16:51:38 +0000

    Move radio.h to dev/radio and tidy-up

--- os/dev/radio/radio.h
+++ os/dev/radio/radio.h
@@ -5,60 +5,61 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-/**
- * \file
- *         Header file for the radio API
- * \author
- *         Adam Dunkels <adam@sics.se>
- *         Joakim Eriksson <joakime@sics.se>
- *         Niclas Finne <nfi@sics.se>
- *         Nicolas Tsiftes <nvt@sics.se>
- */
-
+/*---------------------------------------------------------------------------*/
 /**
  * \addtogroup dev
  * @{
- */
-
-/**
+ *
  * \defgroup radio Radio API
  *
  * The radio API module defines a set of functions that a radio device
  * driver must implement.
  *
  * @{
+ *
+ * \file
+ *         Header file for the radio API.
+ *
+ *         Do not include this file directly. You should include either
+ *         radio-802154.h or radio-ble.h.
+ *
+ * \author
+ *         Adam Dunkels <adam@sics.se>
+ *         Joakim Eriksson <joakime@sics.se>
+ *         Niclas Finne <nfi@sics.se>
+ *         Nicolas Tsiftes <nvt@sics.se>
  */
-
+/*---------------------------------------------------------------------------*/
 #ifndef RADIO_H_
 #define RADIO_H_
+/*---------------------------------------------------------------------------*/
+#include "contiki.h"
 
 #include <stddef.h>
-
+/*---------------------------------------------------------------------------*/
 /**
  * Each radio has a set of parameters that designate the current
  * configuration and state of the radio. Parameters can either have
@@ -198,7 +199,7 @@ enum {
  * To enable both address filter and transmissions of automatic
  * acknowledgments:
  *
- * NETSTACK_RADIO.set_value(RADIO_PARAM_RX_MODE,
+ * NETSTACK_RADIO_802154.set_value(RADIO_PARAM_RX_MODE,
  *       RADIO_RX_MODE_ADDRESS_FILTER | RADIO_RX_MODE_AUTOACK);
  */
 #define RADIO_RX_MODE_ADDRESS_FILTER   (1 << 0)
@@ -214,7 +215,7 @@ enum {
  * RADIO_TX_COLLISION.
  */
 #define RADIO_TX_MODE_SEND_ON_CCA      (1 << 0)
-
+/*---------------------------------------------------------------------------*/
 /* Radio return values when setting or getting radio parameters. */
 typedef enum {
   RADIO_RESULT_OK,
@@ -230,65 +231,8 @@ enum {
   RADIO_TX_COLLISION,
   RADIO_TX_NOACK,
 };
-
-/**
- * The structure of a device driver for a radio in Contiki.
- */
-struct radio_driver {
-
-  int (* init)(void);
-
-  /** Prepare the radio with a packet to be sent. */
-  int (* prepare)(const void *payload, unsigned short payload_len);
-
-  /** Send the packet that has previously been prepared. */
-  int (* transmit)(unsigned short transmit_len);
-
-  /** Prepare & transmit a packet. */
-  int (* send)(const void *payload, unsigned short payload_len);
-
-  /** Read a received packet into a buffer. */
-  int (* read)(void *buf, unsigned short buf_len);
-
-  /** Perform a Clear-Channel Assessment (CCA) to find out if there is
-      a packet in the air or not. */
-  int (* channel_clear)(void);
-
-  /** Check if the radio driver is currently receiving a packet */
-  int (* receiving_packet)(void);
-
-  /** Check if the radio driver has just received a packet */
-  int (* pending_packet)(void);
-
-  /** Turn the radio on. */
-  int (* on)(void);
-
-  /** Turn the radio off. */
-  int (* off)(void);
-
-  /** Get a radio parameter value. */
-  radio_result_t (* get_value)(radio_param_t param, radio_value_t *value);
-
-  /** Set a radio parameter value. */
-  radio_result_t (* set_value)(radio_param_t param, radio_value_t value);
-
-  /**
-   * Get a radio parameter object. The argument 'dest' must point to a
-   * memory area of at least 'size' bytes, and this memory area will
-   * contain the parameter object if the function succeeds.
-   */
-  radio_result_t (* get_object)(radio_param_t param, void *dest, size_t size);
-
-  /**
-   * Set a radio parameter object. The memory area referred to by the
-   * argument 'src' will not be accessed after the function returns.
-   */
-  radio_result_t (* set_object)(radio_param_t param, const void *src,
-                                size_t size);
-
-};
-
+/*---------------------------------------------------------------------------*/
 #endif /* RADIO_H_ */
-
+/*---------------------------------------------------------------------------*/
 /** @} */
 /** @} */
