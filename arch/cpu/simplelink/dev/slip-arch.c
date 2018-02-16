6ce955a71f31f0a190574de268f86395610310a4
Edvard Pettersen <a0132644@ti.com>
2018-02-16 18:50:09 +0100

    Added slip driver

--- arch/cpu/simplelink/dev/slip-arch.c
+++ arch/cpu/simplelink/dev/slip-arch.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (c) 2018, Texas Instruments Incorporated - http://www.ti.com/
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,6 +10,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
+ *
  * 3. Neither the name of the copyright holder nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
@@ -27,58 +28,41 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+/*---------------------------------------------------------------------------*/
 /**
- * \addtogroup cc26xx
- * @{
- *
- * \defgroup cc26xx-uart CC13xx/CC26xx UARTs
- *
- * Driver for the CC13xx/CC26xx UART controller
+ * \addtogroup cc26xx-char-io
  * @{
  *
  * \file
- * Header file for the CC13xx/CC26xx UART driver
+ * Arch-specific SLIP functions for the CC13xx/CC26xx
  */
-#ifndef UART0_ARCH_H_
-#define UART0_ARCH_H_
 /*---------------------------------------------------------------------------*/
-#include <stddef.h>
-#include <stdint.h>
+#include <contiki.h>
+#include <dev/slip.h>
 /*---------------------------------------------------------------------------*/
-typedef int (*uart0_input_cb)(unsigned char);
+#include "dev/uart0-arch.h"
 /*---------------------------------------------------------------------------*/
-/** \name UART functions
- * @{
- */
-
 /**
- * \brief Initializes the UART driver
+ * \brief Write a byte over SLIP
+ * \param c the byte
  */
-void uart0_init(void);
-
+void
+slip_arch_writeb(unsigned char c)
+{
+  uart0_write(&c, 1);
+}
+/*---------------------------------------------------------------------------*/
 /**
- * \brief       Writes data from a memory buffer to the UART interface.
- * \param buffer A pointer to the data buffer.
- * \param size  Size of the data buffer.
- * \return      Number of bytes that has been written to the UART. If an
- *              error occurs, a negative value is returned.
+ * \brief Initialise the arch-specific SLIP driver
  */
-int_fast32_t uart0_write(const void *buffer, size_t size);
-
-/**
- * \brief Reads data from the UART interface to a memory buffer.
- * \param buffer A pointer to the data buffer.
- * \param size  Number of bytes to read
- * \return      Number of bytes that has been written to the buffer. If an
- *              error occurs, a negative value is returned.
- */
-void uart0_set_callback(uart0_input_cb input_cb);
+void
+slip_arch_init()
+{
+  // Enable an input handler. In doing so, the driver will make sure that UART
+  // RX stays operational during deep sleep
+  uart0_init();
+  uart0_set_callback(slip_input_byte);
+}
+/*---------------------------------------------------------------------------*/
 
 /** @} */
-/*---------------------------------------------------------------------------*/
-#endif /* UART0_ARCH_H_ */
-
-/**
- * @}
- * @}
- */
