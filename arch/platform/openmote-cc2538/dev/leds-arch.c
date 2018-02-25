e39472f7d08bc10501949e5ef26af16ddca0c0e9
George Oikonomou <george@contiki-ng.org>
2018-02-25 20:52:32 +0000

    Implement and use the new LED HAL (OpenMote)

--- arch/platform/openmote-cc2538/dev/leds-arch.c
+++ arch/platform/openmote-cc2538/dev/leds-arch.c
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) 2012, Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (c) 2018, George Oikonomou - http://www.spd.gr
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
@@ -26,48 +27,32 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-/*---------------------------------------------------------------------------*/
-/**
- * \addtogroup openmote-cc2538
- * @{
- *
- * \defgroup openmote-leds OpenMote-CC2538 LED driver
- * @{
- *
- * \file
- * LED driver implementation for the OpenMote-CC2538 platform
  */
 /*---------------------------------------------------------------------------*/
 #include "contiki.h"
-#include "reg.h"
 #include "dev/leds.h"
-#include "dev/gpio.h"
+#include "dev/gpio-hal.h"
+
+#include <stdbool.h>
 /*---------------------------------------------------------------------------*/
-#define LEDS_GPIO_PIN_MASK   LEDS_ALL
+const leds_t leds_arch_leds[] = {
+  {
+    .pin = GPIO_PORT_PIN_TO_GPIO_HAL_PIN(LEDS_ARCH_L1_PORT, LEDS_ARCH_L1_PIN),
+    .negative_logic = false
+  },
+  {
+    .pin = GPIO_PORT_PIN_TO_GPIO_HAL_PIN(LEDS_ARCH_L2_PORT, LEDS_ARCH_L2_PIN),
+    .negative_logic = false
+  },
+  {
+    .pin = GPIO_PORT_PIN_TO_GPIO_HAL_PIN(LEDS_ARCH_L3_PORT, LEDS_ARCH_L3_PIN),
+    .negative_logic = false
+  },
+  {
+    .pin = GPIO_PORT_PIN_TO_GPIO_HAL_PIN(LEDS_ARCH_L4_PORT, LEDS_ARCH_L4_PIN),
+    .negative_logic = false
+  },
+};
 /*---------------------------------------------------------------------------*/
-void
-leds_arch_init(void)
-{
-  GPIO_SET_OUTPUT(GPIO_C_BASE, LEDS_GPIO_PIN_MASK);
-}
-/*---------------------------------------------------------------------------*/
-unsigned char
-leds_arch_get(void)
-{
-  return GPIO_READ_PIN(GPIO_C_BASE, LEDS_GPIO_PIN_MASK);
-}
-/*---------------------------------------------------------------------------*/
-void
-leds_arch_set(unsigned char leds)
-{
-  GPIO_WRITE_PIN(GPIO_C_BASE, LEDS_GPIO_PIN_MASK, leds);
-}
-/*---------------------------------------------------------------------------*/
-/**
- * @}
- * @}
- */
+
+
