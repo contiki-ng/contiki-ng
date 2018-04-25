5f0dd15f1ed1431d3678ccacccabea11e3db21b2
Rehan MALAK <rehan.malak@inria.fr>
2018-04-25 14:22:23 +0200

    node killed at the end of 08-native-runs tests
    pgrep pattern shoud be smaller than 15 characters

--- tests/08-native-runs/01-test-data-structures.sh
+++ tests/08-native-runs/01-test-data-structures.sh
@@ -16,7 +16,7 @@ sleep 2
 
 echo "Closing native node"
 sleep 2
-pgrep $CODE | xargs kill -9
+pgrep ${CODE:0:15} | xargs kill -9
 
 if grep -q "=check-me= FAILED" $CODE.log ; then
   echo "==== make.log ====" ; cat make.log;
