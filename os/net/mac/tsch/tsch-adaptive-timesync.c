3d88ba35dd8958e274199857644b406090094bbc
Simon Duquennoy <simon.duquennoy@ri.se>
2018-05-25 12:33:30 -0700

    TSCH: keep track of max/min observed drift and log it periodically

--- os/net/mac/tsch/tsch-adaptive-timesync.c
+++ os/net/mac/tsch/tsch-adaptive-timesync.c
@@ -109,7 +109,9 @@ timesync_learn_drift_ticks(uint32_t time_delta_asn, int32_t drift_ticks)
 
   TSCH_LOG_ADD(tsch_log_message,
       snprintf(log->message, sizeof(log->message),
-          "drift %ld", tsch_adaptive_timesync_get_drift_ppm()));
+          "drift %ld (min/max delta seen: %"PRId32"/%"PRId32")",
+          tsch_adaptive_timesync_get_drift_ppm(),
+          min_drift_seen, max_drift_seen));
 }
 /*---------------------------------------------------------------------------*/
 /* Either reset or update the neighbor's drift */
@@ -136,6 +138,8 @@ tsch_timesync_update(struct tsch_neighbor *n, uint16_t time_delta_asn, int32_t d
       compensated_ticks += drift_correction;
     }
   }
+  min_drift_seen = MIN(drift_correction, min_drift_seen);
+  max_drift_seen = MAX(drift_correction, max_drift_seen);
 }
 /*---------------------------------------------------------------------------*/
 /* Error-accumulation free compensation algorithm */
