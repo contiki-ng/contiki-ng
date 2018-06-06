833cf2f87d8e91fe4ce9d3d801dcacf69b59fb4d
Simon Duquennoy <simon.duquennoy@ri.se>
2018-06-06 15:38:16 +0200

    Fix copyright headers

--- os/services/deployment/deployment.c
+++ os/services/deployment/deployment.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Swedish Institute of Computer Science.
+ * Copyright (c) 2018, RISE SICS.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
