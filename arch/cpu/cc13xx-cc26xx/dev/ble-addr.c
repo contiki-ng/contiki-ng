676b3dbcc199bdb1fadc2d949aa801f67b1408a6
Edvard Pettersen <a0132644@ti.com>
2018-07-06 18:24:35 +0200

    Added BLE and IEEE MAC address functions

--- arch/cpu/cc13xx-cc26xx/dev/ble-addr.c
+++ arch/cpu/cc13xx-cc26xx/dev/ble-addr.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (c) 2016, Michael Spoerk
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- *
  * 3. Neither the name of the copyright holder nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
@@ -27,84 +26,103 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-/*---------------------------------------------------------------------------*/
-/**
- * \addtogroup cc26xx-ieee-addr
- * @{
  *
+ */
+/**
  * \file
- * Driver for the CC13xx/CC26xx IEEE addresses
+ *    Driver for the retrieval of an BLE address from flash
+ *
+ * \author
+ *    Michael Spoerk <mi.spoerk@gmail.com>
  */
 /*---------------------------------------------------------------------------*/
 #include "contiki.h"
+
+#include "dev/ble-hal.h"
 #include "net/linkaddr.h"
 
-#include "ieee-addr.h"
+#include "ble-addr.h"
+
+#include <string.h>
 /*---------------------------------------------------------------------------*/
 #include <ti/devices/DeviceFamily.h>
 #include DeviceFamily_constructPath(inc/hw_memmap.h)
 #include DeviceFamily_constructPath(inc/hw_fcfg1.h)
 #include DeviceFamily_constructPath(inc/hw_ccfg.h)
 /*---------------------------------------------------------------------------*/
-#include <stdint.h>
-#include <string.h>
-/*---------------------------------------------------------------------------*/
-#define IEEE_MAC_PRIMARY_ADDRESS    (FCFG1_BASE + FCFG1_O_MAC_15_4_0)
-#define IEEE_MAC_SECONDARY_ADDRESS  (CCFG_BASE  + CCFG_O_IEEE_MAC_0)
-/*---------------------------------------------------------------------------*/
-#define IEEE_ADDR_SIZE              8
+#define BLE_MAC_PRIMARY_ADDRESS    (FCFG1_BASE + FCFG1_O_MAC_BLE_0)
+#define BLE_MAC_SECONDARY_ADDRESS  (CCFG_BASE  + CCFG_O_IEEE_BLE_0)
 /*---------------------------------------------------------------------------*/
 int
-ieee_addr_cpy_to(uint8_t *dst, uint8_t len)
+ble_addr_cpy(uint8_t *dst)
 {
-  if (len > IEEE_ADDR_SIZE) {
+  if(!dst) {
     return -1;
   }
 
-  if(IEEE_ADDR_CONF_HARDCODED) {
-    const uint8_t ieee_addr_hc[IEEE_ADDR_SIZE] = IEEE_ADDR_CONF_ADDRESS;
+  int i;
 
-    memcpy(dst, &ieee_addr_hc[IEEE_ADDR_SIZE - len], len);
-  } else {
-    int i;
+  const uint8_t * const primary = (uint8_t *)BLE_MAC_PRIMARY_ADDRESS;
+  const uint8_t * const secondary = (uint8_t *)BLE_MAC_SECONDARY_ADDRESS;
 
-    const uint8_t * const primary = (uint8_t *)IEEE_MAC_PRIMARY_ADDRESS;
-    const uint8_t * const secondary = (uint8_t *)IEEE_MAC_SECONDARY_ADDRESS;
+  /* Reading from primary location... */
+  const uint8_t *ble_addr = primary;
 
-    /* Reading from primary location... */
-    const uint8_t *ieee_addr = primary;
-
-    /*
-     * ...unless we can find a byte != 0xFF in secondary
-     *
-     * Intentionally checking all 8 bytes here instead of len, because we
-     * are checking validity of the entire address irrespective of the
-     * actual number of bytes the caller wants to copy over.
-     */
-    for(i = 0; i < IEEE_ADDR_SIZE; i++) {
-      if(HWREG(secondary + i) != 0xFF) {
-        /* A byte in the secondary location is not 0xFF. Use the secondary */
-        ieee_addr = secondary;
-        break;
-      }
-    }
-
-    /*
-     * We have chosen what address to read the address from. Do so,
-     * inverting byte order
-     */
-    for(i = 0; i < len; i++) {
-      dst[i] = HWREG(ieee_addr + len - 1 - i);
+  /*
+   * ...unless we can find a byte != 0xFF in secondary
+   *
+   * Intentionally checking all bytes here instead of len, because we
+   * are checking validity of the entire address irrespective of the
+   * actual number of bytes the caller wants to copy over.
+   */
+  for(i = 0; i < BLE_ADDR_SIZE; i++) {
+    if(HWREG(secondary + i) != 0xFF) {
+      /* A byte in the secondary location is not 0xFF. Use the secondary */
+      ble_addr = secondary;
+      break;
     }
   }
 
-#ifdef IEEE_ADDR_NODE_ID
-  dst[len - 1] = (IEEE_ADDR_NODE_ID >> 0) & 0xFF;
-  dst[len - 2] = (IEEE_ADDR_NODE_ID >> 8) & 0xFF;
-#endif
+  /*
+   * We have chosen what address to read the BLE address from. Do so,
+   * inverting byte order
+   */
+  for(i = 0; i < BLE_ADDR_SIZE; i++) {
+    dst[i] = HWREG(ble_addr + BLE_ADDR_SIZE - 1 - i);
+  }
 
   return 0;
 }
 /*---------------------------------------------------------------------------*/
-/** @} */
+int
+ble_addr_to_eui64(uint8_t *dst, uint8_t *src)
+{
+  if (!dst || !src) {
+    return -1;
+  }
+
+  memcpy(dst, src, 3);
+  dst[3] = 0xFF;
+  dst[4] = 0xFE;
+  memcpy(&dst[5], &src[3], 3);
+
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+int
+ble_addr_to_eui64_cpy(uint8_t *dst)
+{
+  if (!dst) {
+    return -1;
+  }
+
+  int res;
+  uint8_t ble_addr[BLE_ADDR_SIZE];
+
+  res = ble_addr_cpy_to(ble_addr);
+  if (res) {
+    return -1;
+  }
+
+  return ble_addr_to_eui64(dst, ble_addr);
+}
--- arch/cpu/cc13xx-cc26xx/dev/ble-addr.h
+++ arch/cpu/cc13xx-cc26xx/dev/ble-addr.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (c) 2016, Michael Spoerk
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- *
  * 3. Neither the name of the copyright holder nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
@@ -27,62 +26,56 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
-/*---------------------------------------------------------------------------*/
 /**
- * \addtogroup cc26xx
- * @{
- *
- * \defgroup cc26xx-ieee-addr CC13xx/CC26xx IEEE Address Control
- *
- * Driver for the retrieval of an IEEE address from flash
- *
- * The user can specify a hardcoded IEEE address through the
- * IEEE_ADDR_CONF_HARDCODED configuration macro.
- *
- * If the user does not hard-code an address, then one will be read from either
- * the primary location (InfoPage) or from the secondary location (on flash).
- *
- * In order to allow the user to easily program nodes with addresses, the
- * secondary location is given priority: If it contains a valid address then
- * it will be chosen in favour of the one on InfoPage.
- *
- * In this context, an address is valid if at least one of the 8 bytes does not
- * equal 0xFF. If all 8 bytes are 0xFF, then the primary location will be used.
- *
- * In all cases, the address is assumed to be written little-endian.
- *
- * Lastly, it is possible to override the 2 LSB's of the address by using the
- * NODE_ID make variable.
- * @{
- *
  * \file
- * Header file with register and macro declarations for the cc26xx IEEE address
- * driver
+ *    Driver for the retrieval of an BLE address from flash
+ *
+ * \author
+ *    Michael Spoerk <mi.spoerk@gmail.com>
  */
 /*---------------------------------------------------------------------------*/
-#ifndef IEEE_ADDR_H_
-#define IEEE_ADDR_H_
+#ifndef BLE_ADDR_H_
+#define BLE_ADDR_H_
 /*---------------------------------------------------------------------------*/
 #include "contiki.h"
+
+#include <stdint.h>
 /*---------------------------------------------------------------------------*/
 /**
- * \brief Copy the node's IEEE address to a destination memory area
- * \param dst A pointer to the destination area where the IEEE address is to be
+ * \brief Copy the node's factory BLE address to a destination memory area
+ * \param dst A pointer to the destination area where the BLE address is to be
  *            written
- * \param len The number of bytes to write to destination area
  * \return  0 : Returned successfully
  *         -1 : Returned with error
  *
- * This function will copy \e len LS bytes and it will invert byte order in
+ * This function will copy 6 bytes and it will invert byte order in
  * the process. The factory address on devices is normally little-endian,
  * therefore you should expect dst to store the address in a big-endian order.
  */
-int ieee_addr_cpy_to(uint8_t *dst, uint8_t len);
-/*---------------------------------------------------------------------------*/
-#endif /* IEEE_ADDR_H_ */
+int ble_addr_cpy(uint8_t *dst);
 /*---------------------------------------------------------------------------*/
 /**
- * @}
- * @}
+ * \brief Copy the node's BLE address to a destination memory area and converts
+ *      it into a EUI64 address in the process
+ * \param dst A pointer to the destination area where the EUI64 address is to be
+ *            written
+ * \param src A pointer to the BLE address that is to be copied
+ * \return  0 : Returned successfully
+ *         -1 : Returned with error
  */
+int ble_addr_to_eui64(uint8_t *dst, uint8_t *src);
+/*---------------------------------------------------------------------------*/
+/**
+ * \brief Copy the node's EUI64 address that is based on its factory BLE address
+ *      to a destination memory area
+ * \param dst A pointer to the destination area where the EUI64 address is to be
+ *            written
+ * \return  0 : Returned successfully
+ *         -1 : Returned with error
+ */
+int ble_addr_to_eui64_cpy(uint8_t *dst);
+/*---------------------------------------------------------------------------*/
+#endif /* BLE_ADDR_H_ */
+/*---------------------------------------------------------------------------*/
