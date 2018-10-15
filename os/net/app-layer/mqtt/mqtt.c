3b5369cf2a1851a062510e21261e0904704aebba
Simon Duquennoy <simon.duquennoy@ri.se>
2018-10-15 14:11:59 +0200

    MQTT parse_publish_vhdr: added missing initialization of topic_pos

--- os/net/app-layer/mqtt/mqtt.c
+++ os/net/app-layer/mqtt/mqtt.c
@@ -872,6 +872,7 @@ parse_publish_vhdr(struct mqtt_connection *conn,
 
   /* Read out topic length */
   if(conn->in_packet.topic_len_received == 0) {
+    conn->in_packet.topic_pos = 0;
     conn->in_packet.topic_len = (input_data_ptr[(*pos)++] << 8);
     conn->in_packet.byte_counter++;
     if(*pos >= input_data_len) {
