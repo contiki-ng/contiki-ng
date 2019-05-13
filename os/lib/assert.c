94d0dd99e2bfc9f1d6739554cf396fa095d48f03
George Oikonomou <george@contiki-ng.org>
2019-05-13 11:53:22 +0100

    Print some more informative text when assertion fails

--- os/lib/assert.c
+++ os/lib/assert.c
@@ -37,6 +37,8 @@ _xassert(const char *file, int lineno)
   printf("Assertion failed: file %s, line %d.\n", file, lineno);
 
 #if !ASSERT_RETURNS
+  printf("The firmware will stop running\n");
+  printf("A watchdog timer may restart this device\n");
   while(1);
 #endif
 }
