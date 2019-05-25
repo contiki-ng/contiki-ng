2fbb58ebde01b01747743c72fc2e120fc7b7739a
George Oikonomou <george@contiki-ng.org>
2019-05-25 19:08:56 +0100

    Remove remaining traces of NETSTACK_RADIO_MAX_PAYLOAD_LEN

--- os/net/netstack.h
+++ os/net/netstack.h
@@ -108,12 +108,6 @@
 #define NETSTACK_FRAMER   framer_802154
 #endif /* NETSTACK_CONF_FRAMER */
 
-/* Maximal packet length. Each radio driver should define this.
-   When Contiki-NG is compiled for a specific platform (radio), that value is used. */
-#define NETSTACK_RADIO_MAX_PAYLOAD_LEN_XX(radio) radio##_max_payload_len
-#define NETSTACK_RADIO_MAX_PAYLOAD_LEN_X(radio) NETSTACK_RADIO_MAX_PAYLOAD_LEN_XX(radio)
-#define NETSTACK_RADIO_MAX_PAYLOAD_LEN NETSTACK_RADIO_MAX_PAYLOAD_LEN_X(NETSTACK_RADIO)
-
 #include "net/mac/mac.h"
 #include "net/mac/framer/framer.h"
 #include "dev/radio.h"
