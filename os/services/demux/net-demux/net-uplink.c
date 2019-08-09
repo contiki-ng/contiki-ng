ec2e8a204e00c4d34cb56e67a1f0b2f150c7ee6d
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-08-09 14:05:55 +0200

    Add os/services/demux
    - net-demux: demultiplexer between tun and slip
    - mac-demux: demultiplexer between slip and radio (IEEE 802.15.4)

---
+++ os/services/demux/net-demux/net-uplink.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2019, Inria.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * The uplink of the standalone RPL border router is implemented with
+ * the TUN I/F.
+ */
+
+#include <contiki.h>
+
+#include <net/netstack.h>
+
+#include "net-downlink.h"
+
+#include <sys/log.h>
+
+#define LOG_MODULE "6LBR-UP"
+#define LOG_LEVEL LOG_LEVEL_MAIN
+
+extern const struct network_driver tun6_net_driver;
+
+static enum netstack_ip_action ip_input_callback(void);
+static enum netstack_ip_action ip_output_callback(const linkaddr_t *localdest);
+
+bool net_downlink_is_peer_linkaddr(const linkaddr_t *linkaddr);
+
+/*---------------------------------------------------------------------------*/
+static enum netstack_ip_action
+ip_input_callback(void)
+{
+  return NETSTACK_IP_PROCESS;
+}
+/*---------------------------------------------------------------------------*/
+static enum netstack_ip_action
+ip_output_callback(const linkaddr_t *localdest)
+{
+  /*
+   * do nothing but returning NETSTACK_IP_PROCESS so that the
+   * following process is executed.
+   */
+  return NETSTACK_IP_PROCESS;
+}
+/*---------------------------------------------------------------------------*/
+void
+net_uplink_init(void)
+{
+  static struct netstack_ip_packet_processor packet_processor = {
+    .process_input = ip_input_callback,
+    .process_output = ip_output_callback
+  };
+
+  LOG_INFO("Initialize TUN for the uplink\n");
+  tun6_net_driver.init();
+  netstack_ip_packet_processor_add(&packet_processor);
+}
+/*---------------------------------------------------------------------------*/
+void
+net_uplink_output(const linkaddr_t *localdest)
+{
+  const linkaddr_t *localdest_null = NULL;
+  (void)tun6_net_driver.output(localdest_null);
+}
+/*---------------------------------------------------------------------------*/
+bool
+net_uplink_is_peer_linkaddr(const linkaddr_t *linkaddr)
+{
+  return linkaddr != NULL && net_downlink_is_peer_linkaddr(linkaddr) == false;
+}
+/*---------------------------------------------------------------------------*/
