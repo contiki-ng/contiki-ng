d81485e9b720458035ac31b1f002f64a19874dc6
Atis Elsts <atis.elsts@gmail.com>
2019-09-02 21:59:54 +0300

    reduce the IPv6 data structure (DS) maintenance timer frequency

--- os/net/ipv6/uip-ds6.h
+++ os/net/ipv6/uip-ds6.h
@@ -165,7 +165,7 @@
 /** \brief General DS6 definitions */
 /** Period for uip-ds6 periodic task*/
 #ifndef UIP_DS6_CONF_PERIOD
-#define UIP_DS6_PERIOD   (CLOCK_SECOND/10)
+#define UIP_DS6_PERIOD   (60 * CLOCK_SECOND)
 #else
 #define UIP_DS6_PERIOD UIP_DS6_CONF_PERIOD
 #endif
