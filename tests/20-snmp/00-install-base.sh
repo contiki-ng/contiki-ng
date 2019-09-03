ec7f185c82442a6828585a40287286e0bb527a36
Yago Fontoura do Rosario <yago.rosario@hotmail.com.br>
2019-09-03 09:19:13 +0000

    Implemented SNMP-compliance test

---
+++ tests/20-snmp/00-install-base.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source ../utils.sh
+
+# Contiki directory
+CONTIKI=$1
+# Test basename
+BASENAME=$(basename $0 .sh)
+
+sudo apt-get -qq update && sudo apt-get -qq install -y smitools snmp snmp-mibs-downloader
+
+if [ $? -eq 0 ]; then
+    echo "${BASENAME} TEST OK" > ${BASENAME}.testlog
+else
+    echo "${BASENAME} TEST FAIL" > ${BASENAME}.testlog
+fi
+
+# We do not want Make to stop -> Return 0
+# The Makefile will check if a log contains FAIL at the end
+exit 0
