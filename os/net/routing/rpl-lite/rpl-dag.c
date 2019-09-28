d2503024dded921ec953183c4d7a2665d0db6896
Simon Duquennoy <simon.duquennoy@gmail.com>
2019-09-28 13:07:02 +0200

    RPL-Lite: unset preferred parent on global repair prior to init_dag_from_dio, which resets curr_instance

--- os/net/routing/rpl-lite/rpl-dag.c
+++ os/net/routing/rpl-lite/rpl-dag.c
@@ -229,6 +229,7 @@ global_repair_non_root(rpl_dio_t *dio)
     }
     /* Re-initialize configuration from DIO */
     rpl_timers_stop_dag_timers();
+    rpl_neighbor_set_preferred_parent(NULL);
     init_dag_from_dio(dio);
     rpl_local_repair("Global repair");
   }
