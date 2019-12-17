0163ca8676b341535faf2780c1b43e38680e8989
Atis Elsts <atis.elsts@gmail.com>
2019-12-17 14:11:33 +0200

    Add TSCH timeslot element description, based on initial work by Rajeev Pinare

--- os/net/mac/tsch/tsch-timeslot-timing.c
+++ os/net/mac/tsch/tsch-timeslot-timing.c
@@ -47,7 +47,24 @@
 #include "net/mac/tsch/tsch.h"
 
 /**
- * \brief The default timeslot timing in the standard is a guard time of
+ * \brief TSCH timing attributes and description. All timings are in usec.
+ *
+ * CCAOffset   -> time between the beginning of timeslot and start of CCA
+ * CCA         -> duration of CCA (CCA is NOT ENABLED by default)
+ * TxOffset    -> time between beginning of the timeslot and start of frame TX (end of SFD)
+ * RxOffset    -> beginning of the timeslot to when the receiver shall be listening
+ * RxAckDelay  -> end of frame to when the transmitter shall listen for ACK
+ * TxAckDelay  -> end of frame to the start of ACK tx
+ * RxWait      -> time to wait for start of frame (Guard time)
+ * AckWait     -> min time to wait for start of an ACK frame
+ * RxTx        -> receive-to-transmit switch time (NOT USED)
+ * MaxAck      -> TX time to send a max length ACK
+ * MaxTx       -> TX time to send the max length frame
+ *
+ * The TSCH timeslot structure is described in the IEEE 802.15.4-2015 standard,
+ * in particular in the Figure 6-30.
+ *
+ * The default timeslot timing in the standard is a guard time of
  * 2200 us, a Tx offset of 2120 us and a Rx offset of 1120 us.
  * As a result, the listening device has a guard time not centered
  * on the expected Tx time. This is to be fixed in the next iteration
