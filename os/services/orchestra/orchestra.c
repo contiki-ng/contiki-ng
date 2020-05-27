6e538430bf1113e2949e37f253043291e3c79f42
Andreas Urke <arurke@gmail.com>
2020-05-27 20:51:55 +0200

    Use log-module for Orchestra

--- os/services/orchestra/orchestra.c
+++ os/services/orchestra/orchestra.c
@@ -48,8 +48,9 @@
 #include "net/routing/rpl-classic/rpl-private.h"
 #endif
 
-#define DEBUG DEBUG_PRINT
-#include "net/ipv6/uip-debug.h"
+#include "sys/log.h"
+#define LOG_MODULE "Orchestra"
+#define LOG_LEVEL  LOG_LEVEL_MAC
 
 /* A net-layer sniffer for packets sent and received */
 static void orchestra_packet_received(void);
@@ -171,10 +172,10 @@ orchestra_init(void)
   linkaddr_copy(&orchestra_parent_linkaddr, &linkaddr_null);
   /* Initialize all Orchestra rules */
   for(i = 0; i < NUM_RULES; i++) {
-    PRINTF("Orchestra: initializing rule %s (%u)\n", all_rules[i]->name, i);
+    LOG_INFO("Initializing rule %s (%u)\n", all_rules[i]->name, i);
     if(all_rules[i]->init != NULL) {
       all_rules[i]->init(i);
     }
   }
-  PRINTF("Orchestra: initialization done\n");
+  LOG_INFO("Initialization done\n");
 }
