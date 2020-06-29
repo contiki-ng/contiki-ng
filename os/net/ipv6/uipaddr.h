32d1103e165691d6f669ac276ee8ebabaffc0067
alexrayne <alexraynepe196@gmail.com>
2020-06-29 11:18:08 +0300

    * uipaddr.h - decorate doxygen and license

--- os/net/ipv6/uipaddr.h
+++ os/net/ipv6/uipaddr.h
@@ -1,12 +1,56 @@
 /*
- * uipaddr.h
+ * Copyright (c) 2001-2003, Adam Dunkels.
+ * Copyright (c) 2020 alexrayne <alexraynepe196@gmail.com>.
+ * All rights reserved.
  *
- *  Created on: 26 θών. 2020 γ.
- *      Author: netuser
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This file is part of the uIP TCP/IP stack.
+ *
+ *
  */
 
 #ifndef OS_NET_IP_UIPADDR_H_
 #define OS_NET_IP_UIPADDR_H_
+
+/**
+ * \addtogroup uip
+ * @{
+ */
+
+/**
+ * \file
+ * Header file for the uIP TCP/IP stack.
+ * \author  Adam Dunkels <adam@dunkels.com>
+ * \author  Julien Abeille <jabeille@cisco.com> (IPv6 related code)
+ * \author  Mathilde Durvy <mdurvy@cisco.com> (IPv6 related code)
+ * \author  alexrayne <alexraynepe196@gmail.com> (deploy from uip.h)
+ *
+ * Representation of an IP address and related routinues/macro.
+ *
+ */
 
 #include <stdint.h>
 /* For memcmp */
@@ -630,5 +674,7 @@ typedef uip_eth_addr uip_lladdr_t;
    (((a)->u8[15])  == ((b)->u8[15])))
 
 #endif /*NETSTACK_CONF_WITH_IPV6*/
+
+/**  @} */
 
 #endif /* OS_NET_IP_UIPADDR_H_ */
