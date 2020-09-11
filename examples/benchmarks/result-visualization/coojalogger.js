30507ecf9d6b01ff64571214428727595c6307c4
Atis Elsts <atis.elsts@gmail.com>
2020-09-11 12:47:15 +0300

    Add a result visualization example with logfile analysis and plotting script

---
+++ examples/benchmarks/result-visualization/coojalogger.js
@@ -0,0 +1,18 @@
+/* A simple log file generator script */
+
+TIMEOUT(3600000); /* 3600 seconds or 1 hour */
+
+log.log("Starting COOJA logger\n");
+
+timeout_function = function () {
+    log.log("Script timed out.\n");
+    log.testOK();
+}
+
+while (true) {
+    if (msg) {
+        log.log(time + " " + id + " " + msg + "\n");
+    }
+
+    YIELD();
+}
