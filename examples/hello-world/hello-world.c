1510f4095bff7525919fd454457c363eec8340fe
StarUnderNight <1424249401@qq.com>
2020-11-12 22:32:19 +0800

    second

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -54,7 +54,7 @@ PROCESS_THREAD(hello_world_process, ev, data)
   etimer_set(&timer, CLOCK_SECOND * 10);
 
   while(1) {
-    printf("Hello, world----------\n");
+    printf("Hello, world--second--------\n");
 
     /* Wait for the periodic timer to expire and then restart the timer. */
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
