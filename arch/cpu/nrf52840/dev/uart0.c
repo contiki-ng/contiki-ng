8a7e167e2f1f5b36a9ef286150ea39a457848d26
Alex Stanoev <alex@astanoev.com>
2021-03-08 02:44:55 +0000

    nrf52840: Support configurable UART0 baud rate

--- arch/cpu/nrf52840/dev/uart0.c
+++ arch/cpu/nrf52840/dev/uart0.c
@@ -89,7 +89,7 @@ uart0_init(unsigned long ubr)
   nrf_gpio_pin_set(TX_PIN);
   nrf_gpio_cfg_input(RX_PIN, NRF_GPIO_PIN_NOPULL);
 
-  nrf_uart_baudrate_set(UART_INSTANCE, NRF_UART_BAUDRATE_115200);
+  nrf_uart_baudrate_set(UART_INSTANCE, UART0_CONF_BAUD_RATE);
   nrf_uart_configure(UART_INSTANCE, NRF_UART_PARITY_EXCLUDED,
                      NRF_UART_HWFC_DISABLED);
   nrf_uart_txrx_pins_set(UART_INSTANCE, TX_PIN, RX_PIN);
--- arch/cpu/nrf52840/nrf52840-conf.h
+++ arch/cpu/nrf52840/nrf52840-conf.h
@@ -4,6 +4,10 @@
 /*---------------------------------------------------------------------------*/
 #define NETSTACK_CONF_RADIO        nrf52840_ieee_driver
 /*---------------------------------------------------------------------------*/
+#ifndef UART0_CONF_BAUD_RATE
+#define UART0_CONF_BAUD_RATE       NRF_UART_BAUDRATE_115200
+#endif
+/*---------------------------------------------------------------------------*/
 #if NRF52840_NATIVE_USB
 
 #ifndef DBG_CONF_USB
