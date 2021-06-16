13937b842f4aca49c8949c453506210ab54f0793
kkrentz <konrad.krentz@gmail.com>
2021-06-16 17:57:23 +0200

    OpenMote-B: Fixed antenna configuration

--- arch/platform/openmote/openmote-b/board.h
+++ arch/platform/openmote/openmote-b/board.h
@@ -190,7 +190,7 @@
  * @{
  */
 #define ANTENNA_BSP_RADIO_BASE   GPIO_PORT_TO_BASE(GPIO_D_NUM)
-#define ANTENNA_BSP_RADIO_INT    GPIO_PIN_MASK(3)
+#define ANTENNA_BSP_RADIO_INT    GPIO_PIN_MASK(5)
 #define ANTENNA_BSP_RADIO_EXT    GPIO_PIN_MASK(4)
 /** @} */
 /*---------------------------------------------------------------------------*/
