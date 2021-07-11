bf53b1142690b23ec4e9590e933fbb0f312ffbd3
Ninho <ivanilson.junior@dei.uc.pt>
2021-07-11 09:50:54 +0100

    Remove Unused Base Class

--- examples/MetricsSimulation/Model.py
+++ examples/MetricsSimulation/Model.py
@@ -39,37 +39,6 @@ Base = declarative_base(metadata=meta)
 Session = sessionmaker(bind=engine)
 db = Session()
 
-
-'''
-Base model generic class
-'''
-class MyModel():
-    def as_dict(self):
-        return { c.name: getattr(self, c.name) for c in self.__table__.columns }
-    def toJson(self):
-        return json.dumps(self, default=lambda o: o._asdict(), sort_keys=True, indent=4)
-    def _asdict(self):
-        return {c.key: getattr(self, c.key)
-            for c in inspect(self).mapper.column_attrs}
-    def object_to_dict(obj, found=None):
-        if found is None:
-            found = set()
-        mapper = class_mapper(obj.__class__)
-        columns = [column.key for column in mapper.columns]
-        get_key_value = lambda c: (c, getattr(obj, c).isoformat()) if isinstance(getattr(obj, c), datetime) else (c, getattr(obj, c))
-        out = dict(map(get_key_value, columns))
-        for name, relation in mapper.relationships.items():
-            if relation not in found:
-                found.add(relation)
-                related_obj = getattr(obj, name)
-                if related_obj is not None:
-                    if relation.uselist:
-                        out[name] = [object_to_dict(child, found) for child in related_obj]
-                    else:
-                        out[name] = object_to_dict(related_obj, found)
-        return out
-
-
 '''
 Represents an experiment
     Parameters:
