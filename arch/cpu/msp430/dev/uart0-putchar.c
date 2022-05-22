5b013ae714c892d935b6516e0cf19a0f7b5971f8
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2022-05-22 19:55:33 +0200

    z1: add newlib printf support
    This makes 07-hello-world-z1.csc pass
    when using GCC 9.

--- arch/cpu/msp430/dev/uart0-putchar.c
+++ arch/cpu/msp430/dev/uart0-putchar.c
@@ -36,4 +36,19 @@ putchar(int c)
 #endif /* SLIP_ARCH_CONF_ENABLED */
   return c;
 }
+
+#if defined(__GNUC__) && (__GNUC__ >= 9)
+/* The printf() in newlib in GCC 9 from Texas Instruments uses the
+ * "TI C I/O" protocol which is not implemented in GDB. The user manual
+ * suggests overriding write() to redirect printf() output. */
+int
+write(int fd, const char *buf, int len)
+{
+  int i = 0;
+  for(; i < len && buf[i]; i++) {
+    putchar(buf[i]);
+  }
+  return i;
+}
+#endif
 /*---------------------------------------------------------------------------*/
