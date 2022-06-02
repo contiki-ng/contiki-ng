81e08c60ce2e1cb3eb76a225395ed609e34bf478
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2022-06-02 10:38:19 +0200

    tests: use the existing hashbang
    Execute scripts directly so they use
    the specified interpreter + parameters
    in the first line.
    
    This is a sibling to commit 183df607ee5bf9.

--- tests/17-tun-rpl-br/03-border-router-sky.sh
+++ tests/17-tun-rpl-br/03-border-router-sky.sh
@@ -6,4 +6,4 @@ CONTIKI=$1
 # Simulation file
 BASENAME=$(basename $0 .sh)
 
-bash test-border-router.sh $CONTIKI $BASENAME fd00::0212:7404:0004:0404 60
+./test-border-router.sh $CONTIKI $BASENAME fd00::0212:7404:0004:0404 60
