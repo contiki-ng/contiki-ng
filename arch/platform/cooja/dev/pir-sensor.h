a05b9392f529d4de8e014eedbb9408704d705857
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2022-07-03 12:17:25 +0200

    cooja: fix include guard names
    Name the guards according to the filename.

--- arch/platform/cooja/dev/pir-sensor.h
+++ arch/platform/cooja/dev/pir-sensor.h
@@ -28,8 +28,8 @@
  *
  */
 
-#ifndef PIR_H_
-#define PIR_H_
+#ifndef PIR_SENSOR_H_
+#define PIR_SENSOR_H_
 
 #include "lib/sensors.h"
 
@@ -37,4 +37,4 @@ extern const struct sensors_sensor pir_sensor;
 
 #define PIR_SENSOR "PIR"
 
-#endif /* PIR_H_ */
+#endif /* PIR_SENSOR_H_ */
