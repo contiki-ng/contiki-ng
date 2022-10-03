14e18ff2ac1072a5a52fe75e05050ad7b71ac420
Simon Wörner <git@simon-woerner.de>
2022-10-03 09:41:39 +0200

    remove output redirections

--- tests/20-packet-parsing/packet-injector.sh
+++ tests/20-packet-parsing/packet-injector.sh
@@ -17,7 +17,7 @@ echo packet dir = $PACKET_DIR
 
 # Starting Contiki-NG native node
 echo "Starting native node"
-make -C $CODE_DIR TARGET=native > make.log 2> make.err
+make -C $CODE_DIR TARGET=native
 
 for i in $PACKET_DIR/*
 do
@@ -32,7 +32,7 @@ do
     test_files=("$i")
     echo Injecting file $i
   fi
-  timeout -k 1s 2s "$CODE_DIR/$CODE.native" "${test_files[@]}" 2>> $CODE.err
+  timeout -k 1s 2s "$CODE_DIR/$CODE.native" "${test_files[@]}"
   INJECTOR_EXIT_CODE=$?
   echo "exit code:" $INJECTOR_EXIT_CODE
 
@@ -52,25 +52,17 @@ done
 echo "Closing native node"
 
 if [ $((TIMEDOUT + FAILED)) -gt 0 ]; then
-  echo "==== make.log ====" ; cat make.log;
-  echo "==== make.err ====" ; cat make.err;
-  echo "==== $CODE.log ====" ; cat $CODE.log;
-  echo "==== $CODE.err ====" ; cat $CODE.err;
-  printf "%-32s TEST FAIL\n" "$CODE-$TEST_PROTOCOL" | tee $CODE.testlog;
+  printf "%-32s TEST FAIL\n" "$CODE-$TEST_PROTOCOL"
 
   echo "Succeeded: " $SUCCEEDED
   echo "Timed out: " $TIMEDOUT
   echo "Failed   : " $FAILED
 
-  rm -f make.log make.err $CODE.log $CODE.err
   sleep 3
   exit 1
 else
-  cp $CODE.log $CODE.testlog
-  printf "%-32s TEST OK\n" "$CODE-$TEST_PROTOCOL" | tee $CODE.testlog;
+  printf "%-32s TEST OK\n" "$CODE-$TEST_PROTOCOL"
 fi
-
-rm -f make.log make.err $CODE.log $CODE.err
 
 sleep 3
 
