4b6979de4b7c2dd784facad4f49939a3bede67a2
kkrentz <konrad.krentz@gmail.com>
2022-10-05 18:55:26 +0200

    uip: Declares a parameter as const

--- os/net/ipv6/uip-ds6.c
+++ os/net/ipv6/uip-ds6.c
@@ -573,7 +573,7 @@ uip_ds6_select_src(uip_ipaddr_t *src, uip_ipaddr_t *dst)
 
 /*---------------------------------------------------------------------------*/
 void
-uip_ds6_set_addr_iid(uip_ipaddr_t *ipaddr, uip_lladdr_t *lladdr)
+uip_ds6_set_addr_iid(uip_ipaddr_t *ipaddr, const uip_lladdr_t *lladdr)
 {
 #if (UIP_LLADDR_LEN == 8)
   memcpy(ipaddr->u8 + 8, lladdr, UIP_LLADDR_LEN);
