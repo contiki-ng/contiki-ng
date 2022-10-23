098854a42201ae0a6bc308b64021c762803aa132
Joakim Eriksson <ruben.joakim.eriksson@gmail.com>
2022-10-23 17:06:57 +0000

    added robot test example for renode

--- examples/rpl-udp/udp-client.c
+++ examples/rpl-udp/udp-client.c
@@ -14,7 +14,7 @@
 #define UDP_CLIENT_PORT	8765
 #define UDP_SERVER_PORT	5678
 
-#define SEND_INTERVAL		  (60 * CLOCK_SECOND)
+#define SEND_INTERVAL		  (10 * CLOCK_SECOND)
 
 static struct simple_udp_connection udp_conn;
 static uint32_t rx_count = 0;
