ffb8a0bade68e440adde4ea183e89e28f5b6f218
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2023-07-07 12:49:40 +0200

    test-tsl256x: fix typo in comment

--- examples/platform-specific/zoul/test-tsl256x.c
+++ examples/platform-specific/zoul/test-tsl256x.c
@@ -71,7 +71,7 @@ PROCESS_THREAD(remote_tsl256x_process, ev, data)
   PROCESS_BEGIN();
   static int light;
 
-  /* Print the sensor used, teh default is the TSL2561 (from Grove) */
+  /* Print the sensor used, the default is the TSL2561 (from Grove) */
   if(TSL256X_REF == TSL2561_SENSOR_REF) {
     printf("Light sensor test --> TSL2561\n");
   } else if(TSL256X_REF == TSL2563_SENSOR_REF) {
