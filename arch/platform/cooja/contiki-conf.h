e0998121e3aa21762548f1f7880ae11ab559d065
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2023-07-11 11:24:28 +0200

    cooja: remove typedefs for u8_t & friends
    These types are not used in the Cooja
    platform, so remove them.

--- arch/platform/cooja/contiki-conf.h
+++ arch/platform/cooja/contiki-conf.h
@@ -99,12 +99,7 @@
 #define CC_CONF_VA_ARGS                1
 #define CC_CONF_INLINE inline
 
-/* These names are deprecated, use C99 names. */
 #include <inttypes.h>
-typedef uint8_t u8_t;
-typedef uint16_t u16_t;
-typedef uint32_t u32_t;
-typedef int32_t s32_t;
 
 typedef unsigned short uip_stats_t;
 
