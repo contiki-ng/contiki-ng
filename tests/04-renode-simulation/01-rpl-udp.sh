21cfcd891a5373f33102fd475674a6614caa9ec6
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2023-07-23 12:58:52 +0200

    renode tests: remove extra logs
    The test "framework" was switched
    to look at return values instead
    of analyzing logs, so these logs
    are no longer required.

--- tests/04-renode-simulation/01-rpl-udp.sh
+++ tests/04-renode-simulation/01-rpl-udp.sh
@@ -6,14 +6,4 @@ TARGET=cc2538dk
 
 make -C ${TEST_CODE_DIR} clean TARGET=${TARGET}
 make -C ${TEST_CODE_DIR} TARGET=${TARGET}
-
 renode-test --show-log ${TEST_CODE_DIR}/rpl-udp.robot
-
-if [ $? -eq 0 ]; then
-    echo "${TESTNAME} TEST OK" > ${TESTNAME}.testlog
-    make -C ${TEST_CODE_DIR} clean TARGET=${TARGET}
-    exit 0
-else
-    echo "${TESTNAME} TEST FAIL" > ${TESTNAME}.testlog
-    exit 1
-fi
