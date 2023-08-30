9f4ebb8189fcfa3a3e872084fa698fb9b0393d0c
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2023-08-30 16:12:01 +0200

    gecko: add missing parentheses in gpio-hal-arch

--- arch/cpu/gecko/dev/gpio-hal-arch.h
+++ arch/cpu/gecko/dev/gpio-hal-arch.h
@@ -70,7 +70,7 @@
 #define gpio_hal_arch_write_pins(port, pins, value) \
   GPIO_PortOutSetVal(port, value, pins)
 #define gpio_hal_arch_read_pins(port, pins) \
-  (GPIO_PortOutGet(port) & pins)
+  (GPIO_PortOutGet(port) & (pins))
 #define gpio_hal_arch_toggle_pins(port, pins) \
   GPIO_PortOutToggle(port, pins)
 #define gpio_hal_arch_clear_pins(port, pins) \
