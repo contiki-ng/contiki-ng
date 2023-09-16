65e6c7f4a21f9a2d1834e7d528a03d45571f62c7
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2023-09-16 13:58:33 +0200

    Makefile.compile-test: allow running tests
    Add an option to run tests after they are
    compiled, and switch the Renode tests to
    use this functionality.
    
    This puts the cleaning and error detection
    in the Makefile, instead of duplicating the same
    code in every test script.

--- tests/04-renode-simulation/01-rpl-udp.sh
+++ tests/04-renode-simulation/01-rpl-udp.sh
@@ -1,9 +1,3 @@
 #!/bin/sh -e
 
-TESTNAME=01-rpl-udp
-TEST_CODE_DIR=../../examples/rpl-udp
-TARGET=cc2538dk
-
-make -C ${TEST_CODE_DIR} clean TARGET=${TARGET}
-make -C ${TEST_CODE_DIR} TARGET=${TARGET}
-renode-test --show-log ${TEST_CODE_DIR}/rpl-udp.robot
+renode-test --show-log ../../examples/rpl-udp/rpl-udp.robot
