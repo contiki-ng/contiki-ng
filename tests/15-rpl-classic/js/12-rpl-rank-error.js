7ce2fe0131053c19181184b421fa8856ca3847c4
Nicolas Tsiftes <nicolas.tsiftes@ri.se>
2024-02-28 16:56:47 +0100

    Invert test result after the duplicate extension header processing was fixed.

--- tests/15-rpl-classic/js/12-rpl-rank-error.js
+++ tests/15-rpl-classic/js/12-rpl-rank-error.js
@@ -5,13 +5,11 @@ while(true) {
 
   if(msg.contains("RPL Option Error: Dropping Packet")) {
     log.log(time + " " + "node-" + id + " "+ msg + "\n");
-    // FIXME Logic inverted to showcase issue. Revert when implementing fix.
-    log.testOK();
+    log.testFailed();
   }
 
   if(msg.contains("Received all packets")) {
     log.log(time + " " + "node-" + id + " "+ msg + "\n");
-    // FIXME Logic inverted to showcase issue. Revert when implementing fix.
-    log.testFailed();
+    log.testOK();
   }
 }
