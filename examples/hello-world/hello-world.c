77dfba0f6722719687732312857dac1d0da57bdb
Swann Aung <swann1999@gmail.com>
2024-03-01 16:15:20 +0800

    Assignment 1 changes + Create directory for assignment 2

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -54,7 +54,7 @@ PROCESS_THREAD(hello_world_process, ev, data)
   etimer_set(&timer, CLOCK_SECOND * 10);
 
   while(1) {
-    printf("Hello, world\n");
+    printf("Our group members are Mun Le Zong, Swann Tet Aung, Tan Rui Yang and Galvin Chan Shi Yuan\n");
 
     /* Wait for the periodic timer to expire and then restart the timer. */
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
