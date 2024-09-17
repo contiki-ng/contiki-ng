15acbb55ea44bda58972a8643277a907ef6cbf7a
Niclas Finne <niclas.finne@ri.se>
2024-09-17 13:53:34 +0200

    packetbuf: remove unused defines PACKETBUF_ATTRIBUTES / PACKETBUF_ATTR_LAST

--- os/net/packetbuf.h
+++ os/net/packetbuf.h
@@ -270,9 +270,6 @@ void              packetbuf_attr_copyto(struct packetbuf_attr *attrs,
 void              packetbuf_attr_copyfrom(struct packetbuf_attr *attrs,
                                           struct packetbuf_addr *addrs);
 
-#define PACKETBUF_ATTRIBUTES(...) { __VA_ARGS__ PACKETBUF_ATTR_LAST }
-#define PACKETBUF_ATTR_LAST { PACKETBUF_ATTR_NONE, 0 }
-
 #define PACKETBUF_ATTR_BIT  1
 #define PACKETBUF_ATTR_BYTE 8
 #define PACKETBUF_ADDRSIZE (LINKADDR_SIZE * PACKETBUF_ATTR_BYTE)
