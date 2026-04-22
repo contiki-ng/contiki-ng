1c97d5c0f086ac33731e636ea5dbec0f13224792
Joakim Eriksson <joakim.eriksson@ri.se>
2026-04-22 23:18:15 +0200

    nrf54l15: rework 802.15.4 low power timer backend

--- arch/cpu/nrf/nrf54l15/nrf54l15-radio-debug.h
+++ arch/cpu/nrf/nrf54l15/nrf54l15-radio-debug.h
@@ -10,6 +10,11 @@
 
 #include <stdint.h>
 
+#define NRF54L15_BAD_ACK_NONE        0U
+#define NRF54L15_BAD_ACK_CRC_ERROR   1U
+#define NRF54L15_BAD_ACK_PARSE_FAIL  2U
+#define NRF54L15_BAD_ACK_MISMATCH    3U
+
 typedef struct {
   volatile uint32_t rx_frame_ok;
   volatile uint32_t ack_requested;
@@ -21,7 +26,20 @@ typedef struct {
   volatile uint32_t ack_rx_valid;
   volatile uint32_t ack_rx_invalid;
   volatile uint32_t ack_timeout;
+  volatile uint32_t ack_timeout_timer_start;
+  volatile uint32_t ack_timeout_timer_stop;
+  volatile uint32_t ack_timeout_timer_fire;
+  volatile uint32_t ack_timeout_timer_retry;
+  volatile uint32_t ack_timeout_req_accepted;
+  volatile uint32_t ack_timeout_req_rejected;
   volatile uint32_t ack_delay_too_short;
+  volatile uint32_t sl_timer_alarm_callback;
+  volatile uint32_t sl_timer_alarm_pending_seen;
+  volatile uint32_t sl_timer_sync_pending_seen;
+  volatile uint32_t sl_timer_alarm_ccen_fix;
+  volatile uint32_t sl_timer_sync_ccen_fix;
+  volatile uint32_t sl_timer_critical_depth;
+  volatile uint32_t sl_timer_critical_max;
   volatile uint32_t last_ack_irq_entry_cc;
   volatile uint32_t last_ack_core_entry_cc;
   volatile uint32_t last_ack_before_txreq_cc;
@@ -30,6 +48,13 @@ typedef struct {
   volatile uint32_t last_ack_now_cc;
   volatile uint32_t last_ack_fem_cc;
   volatile uint32_t last_ack_arm_result;
+  volatile uint32_t last_bad_ack_reason;
+  volatile uint32_t last_bad_ack_parse_ok;
+  volatile uint32_t last_bad_ack_phr;
+  volatile uint32_t last_bad_ack_fcf0;
+  volatile uint32_t last_bad_ack_fcf1;
+  volatile uint32_t last_bad_ack_dsn;
+  volatile uint32_t last_bad_ack_tx_dsn;
 } nrf54l15_radio_debug_t;
 
 extern nrf54l15_radio_debug_t nrf54l15_radio_debug;
