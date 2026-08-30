d7647239a34bb87e6756a71f764920f3c48f60d9
Nicolas Tsiftes <nicolas.tsiftes@ri.se>
2026-08-30 19:58:13 +0200

    sys: let the dtls log level reach the dtls module
    The module list gave the level of the CoAP module for the DTLS entry, so
    setting the DTLS level at run time changed what CoAP reported and left DTLS
    where it was.

--- os/sys/log.c
+++ os/sys/log.c
@@ -79,7 +79,7 @@ struct log_module all_modules[] = {
   {"framer", &curr_log_level_framer, LOG_CONF_LEVEL_FRAMER},
   {"6top", &curr_log_level_6top, LOG_CONF_LEVEL_6TOP},
   {"coap", &curr_log_level_coap, LOG_CONF_LEVEL_COAP},
-  {"dtls", &curr_log_level_coap, LOG_CONF_LEVEL_DTLS},
+  {"dtls", &curr_log_level_dtls, LOG_CONF_LEVEL_DTLS},
   {"snmp", &curr_log_level_snmp, LOG_CONF_LEVEL_SNMP},
   {"lwm2m", &curr_log_level_lwm2m, LOG_CONF_LEVEL_LWM2M},
   {"sys", &curr_log_level_sys, LOG_CONF_LEVEL_SYS},
